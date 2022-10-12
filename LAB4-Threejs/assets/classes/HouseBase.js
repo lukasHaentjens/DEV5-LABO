578a5697dc57b73f44ac86062e87ac8d9457e420
Lukas Haentjens <lukas.haentjens12@Gmail.com>
2022-10-12 18:12:40 +0200

    Feat: load texture onto houseBase

--- LAB4-Threejs/assets/classes/HouseBase.js
+++ LAB4-Threejs/assets/classes/HouseBase.js
@@ -1,4 +1,6 @@
 import * as THREE from 'three'
+// import texture
+import texture from '../textures/wall-texture.jpg'
 
 export default class HouseBase {
     constructor(x,y,z,size,color) {
@@ -8,7 +10,8 @@ export default class HouseBase {
         this.size = size;
         this.color = color;
         this.geometry = new THREE.BoxGeometry(this.size, this.size, this.size);
-        this.material = new THREE.MeshLambertMaterial({ color: this.color });
+        // use texture
+        this.material = new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load(texture) });
         this.mesh = new THREE.Mesh(this.geometry, this.material);
         this.mesh.position.set(this.x, this.y, this.z);
     }
