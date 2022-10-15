5be6ef077e9bc05b44c113e8c678d98ea5e48204
Lukas Haentjens <lukas.haentjens12@Gmail.com>
2022-10-15 16:49:19 +0200

    Feat: setup api and create variable values for fetch request

--- LAB3-AppPrototype/classes/Asos.js
+++ LAB3-AppPrototype/classes/Asos.js
@@ -3,27 +3,30 @@ import Weather from "./Weather.js";
 export default class Asos {
     constructor(api_key) {
         this.apiKey = api_key;
+        this.lang = 'en-GB';
+        this.location = 'US';
+        this.sizeChart = 'US';
+        this.currency = 'USD';
+        this.itemId = "13600001";
         console.log("Asos class initialized" + this.apiKey);
     }
 
     getClothes() {
         console.log("hello");
 
-        const clothing = document.querySelector('.clothing');
-
         const options = {
             method: 'GET',
             headers: {
-                'X-RapidAPI-Key': this.apiKey,
+                'X-RapidAPI-Key': '8bad7b9159mshe64915501abe39bp17e52bjsn11ce99767a19',
                 'X-RapidAPI-Host': 'asos2.p.rapidapi.com'
             }
         };
 
-        fetch('https://asos2.p.rapidapi.com/v2/auto-complete?q=bikini%20top&store=US&country=US&currency=USD&sizeSchema=US&lang=en-US', options)
+        const url = `https://asos2.p.rapidapi.com/products/v3/detail?id=${this.itemId}&lang=${this.lang}&store=${this.location}&sizeSchema=${this.sizeChart}&currency=${this.currency}`
+
+        fetch(url, options)
             .then(response => response.json())
             .then(response => console.log(response))
             .catch(err => console.error(err));
-
-        clothing.innerHTML = "hello";
     }
 }
