66fdf552b4539fede703dae866d363d179f503a9
Lukas Haentjens <lukas.haentjens12@Gmail.com>
2022-10-15 21:55:17 +0200

    Fix: name of file wrongly written

--- LAB2-ES6/main.js
+++ LAB2-ES6/main.js
@@ -1,5 +1,5 @@
 // import another class
-import Bingo from "./src/bingo.js";
+import Bingo from "./src/Bingo.js";
 
 // kickstart the app by calling the constructor via the `new` keyword
 const bingo = new Bingo();
