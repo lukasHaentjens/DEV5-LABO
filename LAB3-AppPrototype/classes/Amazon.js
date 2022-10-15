50b67397d292ce80dba3aa7ce26059ccbaac7af2
Lukas Haentjens <lukas.haentjens12@Gmail.com>
2022-10-15 18:45:39 +0200

    Fix: class name; Feat: search on keyword fetch call and display image

--- LAB3-AppPrototype/classes/Amazon.js
+++ LAB3-AppPrototype/classes/Amazon.js
@@ -5,8 +5,37 @@ export default class Amazon {
         this.apiKey = api_key;
         console.log("Asos class initialized" + this.apiKey);
     }
-    getClothes() {
+
+    getClothes(weather) {
         console.log("hello");
+
+        switch (weather) {
+            case "Sunny":
+                console.log("Sunny");
+                break;
+            case "Partly cloudy":
+                console.log("Partly cloudy");
+                break;
+            case "Cloudy":
+                console.log("Cloudy");
+                break;
+            case "Light rain":
+                console.log("Light rain");
+                break;
+            case "Moderate rain":
+                console.log("Moderate rain");
+                break;
+            case "Heavy rain":
+                console.log("Heavy rain");
+                break;
+            case "Overcast":
+                console.log("Overcast");
+                break;
+            default:
+                console.log("Default");
+                break;
+        }
+
         const options = {
             method: 'GET',
             headers: {
@@ -15,9 +44,14 @@ export default class Amazon {
             }
         };
 
-        fetch('https://amazon-product-reviews-keywords.p.rapidapi.com/product/details?asin=B07ZPKR714&country=US', options)
+        fetch(`https://amazon-product-reviews-keywords.p.rapidapi.com/product/search?keyword=iphone&country=US&category=aps`, options)
             .then(response => response.json())
-            .then(data => console.log(data.product.title))
+            .then(data => {
+                let imgSrc = data.products[0].thumbnail;
+                console.log(imgSrc);
+                const img = document.querySelector('.amazon--img');
+                img.src = imgSrc;
+            })
             .catch(err => console.error(err));
     }
 }
