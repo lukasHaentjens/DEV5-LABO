2f97c5b3190230b072d870b5c435ba0f27398ee9
Lukas Haentjens <lukas.haentjens12@Gmail.com>
2022-11-16 13:32:45 +0100

    Initial setup

---
+++ LAB6-VueJS/src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref } from 'vue'
+import 'animate.css';
+import VideoDetails from './components/VideoDetails.vue'
+import VideoPlayer from './components/VideoPlayer.vue'
+</script>
+
+<template>
+  <div class="app">
+    <VideoPlayer />
+    <div class="sidebar">
+      <VideoDetails />
+      <Message />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+/* create grid */
+.app {
+  display: grid;
+  grid-template-columns: 3fr 8fr;
+}
+/* elements in different columns */
+VideoPlayer {
+  grid-column: 1;
+}
+.sidebar {
+  grid-column: 2;
+  display: flex;
+  flex-flow: column;
+  height: 100%;
+}
+
+</style>
---
+++ LAB6-VueJS/src/components/VideoDetails.vue
@@ -0,0 +1,39 @@
+<script setup>
+
+import { ref, onMounted, reactive } from "vue";
+
+// Ref is two way binding, past direct iets aan in beeld (handig in chat apps)
+let username = ref('')
+let description = ref('')
+
+// onmounted
+onMounted(()=> {
+  // fetch api
+  const api_url = "http://localhost:5173/tiktok.json";
+  fetch(api_url)
+      .then(response => response.json())
+      .then(data => {
+        // set data to state
+        console.log(data);
+        username.value = data.videos[0].username;
+        description.value = data.videos[0].description;
+      })
+})
+
+
+</script>
+<template>
+  <div>
+    <h3>
+      {{ username }}
+    </h3>
+    <p>
+      {{ description }}
+    </p>
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
