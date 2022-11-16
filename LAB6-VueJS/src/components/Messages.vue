d757107c549d8134205cf3a8d53719065025a956
Lukas Haentjens <lukas.haentjens12@Gmail.com>
2022-11-16 13:36:14 +0100

    Feat: add structure to comment component

--- LAB6-VueJS/src/components/Messages.vue
+++ LAB6-VueJS/src/components/Messages.vue
@@ -3,7 +3,19 @@
 </script>
 
 <template>
-
+  <div class="comments">
+    <div class="comments__header">
+      <h3>Comments</h3>
+      <div class="comment">
+        <ul>
+          <li>
+            <h4>Username</h4>
+            <p>Comment</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
